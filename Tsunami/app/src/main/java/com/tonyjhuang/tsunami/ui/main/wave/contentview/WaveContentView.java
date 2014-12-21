bac45d9deacdb75749443c06630a201115c2153f
tony huang <tony.huang.jun@gmail.com>
2014-12-20 21:45:11 -0500

    added comments

--- Tsunami/app/src/main/java/com/tonyjhuang/tsunami/ui/main/wave/contentview/WaveContentView.java
+++ Tsunami/app/src/main/java/com/tonyjhuang/tsunami/ui/main/wave/contentview/WaveContentView.java
@@ -10,24 +10,61 @@ import com.tonyjhuang.tsunami.ui.main.wave.WavePresenter;
  */
 public interface WaveContentView {
 
+    /**
+     * Set the view controller for this WaveContentView
+     */
     public void setPresenter(WavePresenter presenter);
 
+    /**
+     * Set the current viewtype to SPLASHING and display the splash card to the user
+     */
     public void showSplashCard();
 
+    /**
+     * Set the current viewtype to CONTENT and display a wave in the content cardview
+     */
     public void showContentCard(Wave wave);
 
+    /**
+     * Are we showing the splash card?
+     */
     public boolean isShowingSplashCard();
 
-    public boolean isShowingContentCard();
-
+    /**
+     * Get the current wave that we're showing. If this view is currently splashing, this method
+     * should return null.
+     */
     public Wave getContentWave();
 
+    /**
+     * Retrieve all user input from the splash card.
+     */
     public SplashCard.SplashContent retrieveSplashContent();
 
+    /**
+     * Remove all user input from the splash card
+     */
     public void clearSplashCard();
 
+    /**
+     * Scroll the cardview up offscreen.
+     */
+    public void scrollCardUp();
+
+    /**
+     * Scroll the cardview down offscreen.
+     */
+    public void scrollCardDown();
+
+    /**
+     * Set a custom OnScrollListener for this view that will be notified of scroll events.
+     */
     public void setOnScrollListener(OnScrollListener onScrollListener);
 
+    /**
+     * Set a ViewTypeChangedListener for this view which will be notified when this view goes
+     * from splashing to content and vice versa
+     */
     public void setOnViewTypeChangedListener(OnViewTypeChangedListener onViewTypeChangedListener);
 
     public static interface OnScrollListener {
