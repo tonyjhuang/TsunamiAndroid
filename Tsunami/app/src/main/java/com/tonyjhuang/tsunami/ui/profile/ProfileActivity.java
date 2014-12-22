d5c16414e925c82c021a1e42a381ccfe3cf77739
tony huang <tony.huang.jun@gmail.com>
2014-12-22 00:25:11 -0500

    fading actionbar, variable sized coverimage

--- Tsunami/app/src/main/java/com/tonyjhuang/tsunami/ui/profile/ProfileActivity.java
+++ Tsunami/app/src/main/java/com/tonyjhuang/tsunami/ui/profile/ProfileActivity.java
@@ -1,26 +1,45 @@
 package com.tonyjhuang.tsunami.ui.profile;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.tonyjhuang.tsunami.R;
 import com.tonyjhuang.tsunami.TsunamiActivity;
-import com.tonyjhuang.tsunami.utils.TsunamiConstants;
 import com.tonyjhuang.tsunami.injection.ProfileModule;
+import com.tonyjhuang.tsunami.logging.Timber;
+import com.tonyjhuang.tsunami.ui.customviews.scrollview.ObservableScrollView;
+import com.tonyjhuang.tsunami.ui.customviews.scrollview.OnScrollListener;
+import com.tonyjhuang.tsunami.utils.TsunamiConstants;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.InjectView;
 
 /**
  * Created by tony on 12/21/14.
  */
-public class ProfileActivity extends TsunamiActivity {
+public class ProfileActivity extends TsunamiActivity implements OnScrollListener {
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
+    @InjectView(R.id.scrollview)
+    ObservableScrollView scrollView;
+    @InjectView(R.id.cover)
+    ImageView coverImage;
+
+    private static int coverPhotoIndex;
+    static {
+        coverPhotoIndex = new Random().nextInt(4);
+    }
 
     public static void startProfileActivity(TsunamiActivity activity) {
         activity.startActivityForResult(
@@ -30,9 +49,17 @@ public class ProfileActivity extends TsunamiActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setElevation(0);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        setToolbarBackgroundAlpha(0);
+
+        scrollView.setOnScrollListener(this);
+
+        setCoverImageHeight((int) (getScreenDimensions().y / 3.0f));
     }
 
     @Override
@@ -45,5 +72,36 @@ public class ProfileActivity extends TsunamiActivity {
      */
     private void setToolbarBackgroundAlpha(int alpha) {
         toolbar.getBackground().setAlpha(alpha);
+    }
+
+    @Override
+    public void onScrollChanged(int l, int t, int oldl, int oldt) {
+        int toolbarHeight = toolbar.getHeight();
+        int ratio = (int) (((float) Math.min(t, toolbarHeight)) / toolbarHeight * 255);
+        setToolbarBackgroundAlpha(ratio);
+    }
+
+    private Point getScreenDimensions() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size;
+    }
+
+    private void setCoverImageHeight(int height) {
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) coverImage.getLayoutParams();
+        layoutParams.height = height;
+        coverImage.setLayoutParams(layoutParams);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
