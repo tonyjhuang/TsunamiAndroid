9ceaa7c8c680188f0ff30a94e8b27b02ff4e328b
tony huang <tony.huang.jun@gmail.com>
2014-12-21 23:26:16 -0500

    added image resources, replaced current location asset

--- Tsunami/app/src/main/java/com/tonyjhuang/tsunami/ui/profile/ProfileActivity.java
+++ Tsunami/app/src/main/java/com/tonyjhuang/tsunami/ui/profile/ProfileActivity.java
@@ -2,7 +2,9 @@ package com.tonyjhuang.tsunami.ui.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
+import com.tonyjhuang.tsunami.R;
 import com.tonyjhuang.tsunami.TsunamiActivity;
 import com.tonyjhuang.tsunami.utils.TsunamiConstants;
 import com.tonyjhuang.tsunami.injection.ProfileModule;
@@ -10,10 +12,15 @@ import com.tonyjhuang.tsunami.injection.ProfileModule;
 import java.util.Arrays;
 import java.util.List;
 
+import butterknife.InjectView;
+
 /**
  * Created by tony on 12/21/14.
  */
 public class ProfileActivity extends TsunamiActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
 
     public static void startProfileActivity(TsunamiActivity activity) {
         activity.startActivityForResult(
@@ -23,11 +30,20 @@ public class ProfileActivity extends TsunamiActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setSupportActionBar(toolbar);
         getSupportActionBar().setElevation(0);
     }
 
     @Override
     protected List<Object> getMyModules() {
         return Arrays.asList(new ProfileModule());
+    }
+
+    /**
+     * @param alpha 0 is transparent, 255 is opaque
+     */
+    private void setToolbarBackgroundAlpha(int alpha) {
+        toolbar.getBackground().setAlpha(alpha);
     }
 }
